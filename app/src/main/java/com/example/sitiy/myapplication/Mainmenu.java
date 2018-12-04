998fde87f4b56ab2200fe2700359699587bba202
sitiymaryam <sitimaryam.razaq@gmail.com>
2018-12-04 23:54:29 +0700

    Memperbaiki button login dan nav bar menu

--- app/src/main/java/com/example/sitiy/myapplication/Mainmenu.java
+++ app/src/main/java/com/example/sitiy/myapplication/Mainmenu.java
@@ -80,17 +80,17 @@ public class Mainmenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.beranda) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.dpa) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.matkul) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.filesaya) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.pengumuman) {
 
         }
 
